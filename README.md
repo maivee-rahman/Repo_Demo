3b3f723cfe4a5484cfa61eec03169fac86f360c4
My Bee <mdmaiveerahman@gmail.com>
2026-02-28 22:15:09 +0600

    add python file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Repo_Demo
 This is a Demo of Git &amp; Github Class.
+
+# Teacher:
+Shradha Khapra
+
+# Student:
+Md Maivee
---
+++ demo.py
@@ -0,0 +1,5 @@
+def repo_details(name):
+    print("This is a repo file, made by: ",name)
+    print("He is a AI/ML engineering Student.Currently learning DL.\nThank You!")
+
+repo_details("Maivee")
